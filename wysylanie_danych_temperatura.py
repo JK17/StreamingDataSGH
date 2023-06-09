209538f9eefafd953aa3767ba26e647599ecdae5
Jan Kwiatkowski <jaskoo2@gmail.com>
2023-06-09 16:52:18 +0200

    change temp func

--- wysylanie_danych_temperatura.py
+++ wysylanie_danych_temperatura.py
@@ -15,11 +15,29 @@ import numpy as np
 TOPIC = "test2"
 
 def prepare_temperature():
+    
     number_of_points = 100
     center = 60
     scale = 5
-    x = np.linspace(0, 50, number_of_points)
-    y = [y for y in 40 * np.sin(x)+ center +np.random.normal(0, scale ,number_of_points)]
+
+    heating_duration = 70  # Czas trwania fazy grzewczej w iteracjach
+    cooling_duration = 100  # Czas trwania fazy chłodzenia w iteracjach
+    x = np.linspace(0, number_of_points, number_of_points)
+    y = [y for y in 40 * np.sin(x * (np.pi / heating_duration)) + center + np.random.normal(0, scale, number_of_points)]
+    y = [temp if temp <= 95 else 94 + np.random.random() for temp in y]
+    y = [temp if temp >= 25 else 25 + np.random.random() for temp in y]
+
+ 
+
+    # Symulowanie fazy grzewczej i chłodzenia
+    for i in range(1, len(y) - 1):
+        if i % (heating_duration + cooling_duration) < heating_duration:
+            y[i] += np.random.normal(0, 2)  # Dodatkowy wzrost temperatury w fazie grzewczej
+            y[i] = max(y[i-1], y[i])  # Utrzymywanie wartości rosnącej
+        else:
+            cooling_factor = np.random.uniform(0.8, 1.0)  # Współczynnik chłodzenia
+            y[i] = y[i-1] * cooling_factor  # Spadek temperatury w fazie chłodzenia
+
     return y
 
 def acked(err, msg):
